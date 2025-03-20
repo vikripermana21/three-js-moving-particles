1b873756e98efe488d726b6fe997c90987fde4bd
Vikri Permana <vikripermana91@gmail.com>
2025-03-20 22:27:25 +0700

    :fire: Disable Orbit Control

--- src/script.js
+++ src/script.js
@@ -1,218 +1,254 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import particlesVertexShader from './shaders/particles/vertex.glsl'
-import particlesFragmentShader from './shaders/particles/fragment.glsl'
-import GUI from 'lil-gui'
+import * as THREE from "three";
+import particlesVertexShader from "./shaders/particles/vertex.glsl";
+import particlesFragmentShader from "./shaders/particles/fragment.glsl";
+import GUI from "lil-gui";
 
 /**
  * Base
  */
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 // Loaders
-const textureLoader = new THREE.TextureLoader()
+const textureLoader = new THREE.TextureLoader();
 
 // GUI
-const gui = new GUI()
+const gui = new GUI();
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight,
-    pixelRatio: Math.min(window.devicePixelRatio, 2)
-}
+  width: window.innerWidth,
+  height: window.innerHeight,
+  pixelRatio: Math.min(window.devicePixelRatio, 2),
+};
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-    sizes.pixelRatio = Math.min(window.devicePixelRatio, 2)
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  sizes.pixelRatio = Math.min(window.devicePixelRatio, 2);
 
-    // Materials
-    particlesMaterial.uniforms.uResolution.value.set(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)
+  // Materials
+  particlesMaterial.uniforms.uResolution.value.set(
+    sizes.width * sizes.pixelRatio,
+    sizes.height * sizes.pixelRatio
+  );
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(sizes.pixelRatio)
-})
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(sizes.pixelRatio);
+});
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(0, 0, 18)
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+  35,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.set(0, 0, 18);
+scene.add(camera);
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+// const controls = new OrbitControls(camera, canvas);
+// controls.enableDamping = true;
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    antialias: true
-})
-renderer.setClearColor('#181818')
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(sizes.pixelRatio)
+  canvas: canvas,
+  antialias: true,
+});
+renderer.setClearColor("#181818");
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(sizes.pixelRatio);
 
 /*
-* Displacement
-*/
-const displacement = {}
+ * Displacement
+ */
+const displacement = {};
 
 // 2D Canvas
-displacement.canvas = document.createElement("canvas")
-displacement.canvas.height = 128
-displacement.canvas.width = 128
-displacement.canvas.style.position = 'fixed'
-displacement.canvas.style.width = '256px'
-displacement.canvas.style.height = '256px'
-displacement.canvas.style.top = 0
-displacement.canvas.style.left = 0
-displacement.canvas.style.zIndex = -10
+displacement.canvas = document.createElement("canvas");
+displacement.canvas.height = 128;
+displacement.canvas.width = 128;
+displacement.canvas.style.position = "fixed";
+displacement.canvas.style.width = "256px";
+displacement.canvas.style.height = "256px";
+displacement.canvas.style.top = 0;
+displacement.canvas.style.left = 0;
+displacement.canvas.style.zIndex = -10;
 
 // Context
-displacement.context = displacement.canvas.getContext("2d")
-displacement.context.fillRect(0,0,displacement.canvas.width,displacement.canvas.height)
+displacement.context = displacement.canvas.getContext("2d");
+displacement.context.fillRect(
+  0,
+  0,
+  displacement.canvas.width,
+  displacement.canvas.height
+);
 
 // Glow image
-displacement.glowImage = new Image()
-displacement.glowImage.src = "./glow.png"
-document.body.append(displacement.canvas)
+displacement.glowImage = new Image();
+displacement.glowImage.src = "./glow.png";
+document.body.append(displacement.canvas);
 
 // Interactive Plane
 displacement.interactivePlane = new THREE.Mesh(
-    new THREE.PlaneGeometry(10,10),
-    new THREE.MeshBasicMaterial({color:"red", side:THREE.DoubleSide})
-)
-displacement.interactivePlane.visible = false
+  new THREE.PlaneGeometry(10, 10),
+  new THREE.MeshBasicMaterial({ color: "red", side: THREE.DoubleSide })
+);
+displacement.interactivePlane.visible = false;
 
 // Texture
-displacement.texture = new THREE.CanvasTexture(displacement.canvas)
+displacement.texture = new THREE.CanvasTexture(displacement.canvas);
 
-scene.add(displacement.interactivePlane)
+scene.add(displacement.interactivePlane);
 
 // Raycaster
-displacement.raycaster = new THREE.Raycaster()
+displacement.raycaster = new THREE.Raycaster();
 
 // Cursor Position
-displacement.screenCursor = new THREE.Vector2(9999,9999)
-displacement.canvasCursor = new THREE.Vector2(9999,9999)
-displacement.canvasCursorPrevious = new THREE.Vector2(9999,9999)
+displacement.screenCursor = new THREE.Vector2(9999, 9999);
+displacement.canvasCursor = new THREE.Vector2(9999, 9999);
+displacement.canvasCursorPrevious = new THREE.Vector2(9999, 9999);
 window.addEventListener("pointermove", (e) => {
-    displacement.screenCursor.x =( e.clientX / sizes.width) * 2 - 1
-    displacement.screenCursor.y = - (e.clientY / sizes.height) * 2 + 1
-})
+  displacement.screenCursor.x = (e.clientX / sizes.width) * 2 - 1;
+  displacement.screenCursor.y = -(e.clientY / sizes.height) * 2 + 1;
+});
 
 /*
-* Parameters
-*/
-const parameters = {}
+ * Parameters
+ */
+const parameters = {};
 parameters.spread = 0.5;
 
 /**
  * Particles
  */
-const particlesGeometry = new THREE.PlaneGeometry(10, 10, 128, 128)
-particlesGeometry.setIndex(null)
+const particlesGeometry = new THREE.PlaneGeometry(10, 10, 128, 128);
+particlesGeometry.setIndex(null);
 
-const intensitiesArray = new Float32Array(particlesGeometry.attributes.position.count)
-const anglesArray = new Float32Array(particlesGeometry.attributes.position.count)
+const intensitiesArray = new Float32Array(
+  particlesGeometry.attributes.position.count
+);
+const anglesArray = new Float32Array(
+  particlesGeometry.attributes.position.count
+);
 
-for(let i=0; i < particlesGeometry.attributes.position.count; i++){
-    intensitiesArray[i] = Math.random()
-    anglesArray[i] = Math.random() * Math.PI * 2
+for (let i = 0; i < particlesGeometry.attributes.position.count; i++) {
+  intensitiesArray[i] = Math.random();
+  anglesArray[i] = Math.random() * Math.PI * 2;
 }
 
-particlesGeometry.setAttribute("aIntensity",new THREE.BufferAttribute(intensitiesArray,1))
-particlesGeometry.setAttribute("aRandomAngles",new THREE.BufferAttribute(anglesArray,1))
+particlesGeometry.setAttribute(
+  "aIntensity",
+  new THREE.BufferAttribute(intensitiesArray, 1)
+);
+particlesGeometry.setAttribute(
+  "aRandomAngles",
+  new THREE.BufferAttribute(anglesArray, 1)
+);
 
 const particlesMaterial = new THREE.ShaderMaterial({
-    vertexShader: particlesVertexShader,
-    fragmentShader: particlesFragmentShader,
-    blending: THREE.AdditiveBlending,
-    uniforms:
-    {
-        uResolution: new THREE.Uniform(new THREE.Vector2(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)),
-    
-        uPictureTexture: new THREE.Uniform(textureLoader.load("./picture-4.png")),
-        uDisplacementTexture: new THREE.Uniform(displacement.texture),
+  vertexShader: particlesVertexShader,
+  fragmentShader: particlesFragmentShader,
+  blending: THREE.AdditiveBlending,
+  uniforms: {
+    uResolution: new THREE.Uniform(
+      new THREE.Vector2(
+        sizes.width * sizes.pixelRatio,
+        sizes.height * sizes.pixelRatio
+      )
+    ),
 
-        uSpread: new THREE.Uniform(parameters.spread)
-    }
-})
-const particles = new THREE.Points(particlesGeometry, particlesMaterial)
-scene.add(particles)
+    uPictureTexture: new THREE.Uniform(textureLoader.load("./picture-4.png")),
+    uDisplacementTexture: new THREE.Uniform(displacement.texture),
 
-gui.add(parameters,"spread").min(0).max(1).onChange((e) => {
-    particlesMaterial.uniforms.uSpread.value = e
-})
+    uSpread: new THREE.Uniform(parameters.spread),
+  },
+});
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particles);
+
+gui
+  .add(parameters, "spread")
+  .min(0)
+  .max(1)
+  .onChange((e) => {
+    particlesMaterial.uniforms.uSpread.value = e;
+  });
 
 /**
  * Animate
  */
-const tick = () =>
-{
-    // Update controls
-    controls.update()
-    
-    displacement.raycaster.setFromCamera(displacement.screenCursor,camera)
-    const intersections = displacement.raycaster.intersectObject(displacement.interactivePlane)
-    
-    if(intersections.length){
-        const uv = intersections[0].uv
-        
-        displacement.canvasCursor.x = uv.x * displacement.canvas.width
-        displacement.canvasCursor.y = (1-uv.y) * displacement.canvas.height
-    }
+const tick = () => {
+  // Update controls
 
-    // Speed Alpha
-    const speed = displacement.canvasCursorPrevious.distanceTo(displacement.canvasCursor)
-    displacement.canvasCursorPrevious.copy(displacement.canvasCursor)
+  displacement.raycaster.setFromCamera(displacement.screenCursor, camera);
+  const intersections = displacement.raycaster.intersectObject(
+    displacement.interactivePlane
+  );
 
-    const alpha = Math.min(speed * 0.1,1)
+  if (intersections.length) {
+    const uv = intersections[0].uv;
 
-    // Draw Image
+    displacement.canvasCursor.x = uv.x * displacement.canvas.width;
+    displacement.canvasCursor.y = (1 - uv.y) * displacement.canvas.height;
+  }
 
-        // Fade out
-        displacement.context.globalCompositeOperation = 'source-over'
-        displacement.context.globalAlpha = 0.02
-        displacement.context.fillRect(0, 0, displacement.canvas.width, displacement.canvas.height)
+  // Speed Alpha
+  const speed = displacement.canvasCursorPrevious.distanceTo(
+    displacement.canvasCursor
+  );
+  displacement.canvasCursorPrevious.copy(displacement.canvasCursor);
 
-    const glowSize = displacement.canvas.width * 0.25
-    displacement.context.globalCompositeOperation = 'lighten'
-    displacement.context.globalAlpha = alpha
-    displacement.context.drawImage(
-        displacement.glowImage,
-        displacement.canvasCursor.x - glowSize * 0.5,
-        displacement.canvasCursor.y - glowSize * 0.5,
-        glowSize,
-        glowSize
-    )
+  const alpha = Math.min(speed * 0.1, 1);
 
-    // Texture
-    displacement.texture.needsUpdate = true
+  // Draw Image
 
-    // Render
-    renderer.render(scene, camera)
+  // Fade out
+  displacement.context.globalCompositeOperation = "source-over";
+  displacement.context.globalAlpha = 0.02;
+  displacement.context.fillRect(
+    0,
+    0,
+    displacement.canvas.width,
+    displacement.canvas.height
+  );
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
+  const glowSize = displacement.canvas.width * 0.25;
+  displacement.context.globalCompositeOperation = "lighten";
+  displacement.context.globalAlpha = alpha;
+  displacement.context.drawImage(
+    displacement.glowImage,
+    displacement.canvasCursor.x - glowSize * 0.5,
+    displacement.canvasCursor.y - glowSize * 0.5,
+    glowSize,
+    glowSize
+  );
 
-tick()
+  // Texture
+  displacement.texture.needsUpdate = true;
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
